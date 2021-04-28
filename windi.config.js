4833b52abf929958f2a1b590659cf4108ad55419
yuler <joyful4yu@gmail.com>
2021-04-28 23:13:54 +0800

    [mp]: Add `windi` dep instead of `tailwind`

---
+++ windi.config.js
@@ -0,0 +1,10 @@
+// refs: https://windicss.org/guide/configuration.html
+
+module.exports = {
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    screens: {},
+  },
+  variants: {},
+  plugins: [],
+}
