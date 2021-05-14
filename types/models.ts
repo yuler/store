bd9c9f20ff6798b37df0648047c67137a5ce2350
yuler <joyful4yu@gmail.com>
2021-05-15 01:03:20 +0800

    Define some type for models

---
+++ types/models.ts
@@ -0,0 +1,40 @@
+interface BaseModel {
+  createdAt: Date
+  updatedAt: Date
+  deleted: boolean
+}
+
+export interface User extends BaseModel {
+  nickname: string
+  gender: number
+  city: string
+}
+
+export interface Porduct extends BaseModel {
+  name: string
+  title: string
+  brand: string
+  provider: string
+  price: number
+  markingPrice: number
+  thumbnail: string
+  description: string
+  carousels: string[]
+
+  createdAt: Date
+  updatedAt: Date
+  deleted: boolean
+}
+
+export interface Category extends BaseModel {
+  name: string
+}
+
+export interface Label extends BaseModel {
+  name: string
+}
+
+export interface Order extends BaseModel {
+  userId: string
+  orderNO: string
+}
