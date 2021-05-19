a18368ce2b89d5713e550b535ddbd154e3af01ab
yuler <joyful4yu@gmail.com>
2021-05-19 11:37:24 +0800

    Update ci workflow & Code style format

--- mp/windi.config.cjs
+++ mp/windi.config.cjs
@@ -1,21 +1,11 @@
 // Refs: https://windicss.org/guide/configuration.html
 // @ts-check
 
-const { defineConfig } = require('windicss/helpers')
-
-// // define spacing
-// const spaces = [
-// 	80,
-// 	100
-// ]
-// const spacing = {}
-// for (const space of spaces) {
-// 	spacing[space] = `${space}rpx`
-// }
+const {defineConfig} = require('windicss/helpers');
 
 module.exports = defineConfig({
 	darkMode: 'class', // Or 'media' or 'class'
 	theme: {
-		screens: {},
+		screens: {}
 	}
-})
+});
