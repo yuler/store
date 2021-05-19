ab0d90500d703738642122b9100a4db0625cb4b8
yuler <joyful4yu@gmail.com>
2021-05-19 19:52:20 +0800

    Add `@yuler/utils` dep & Use debounce wrap watch file change

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,5 +1,6 @@
 import {spawn} from 'node:child_process';
 import {watch} from 'chokidar';
+import {debounce} from '@yuler/utils';
 
 // Watch ts
 spawn('npx', ['tsc', '-w'], {stdio: 'inherit'});
@@ -8,7 +9,10 @@ spawn('npx', ['tsc', '-w'], {stdio: 'inherit'});
 watch(['mp/**/*.wxml', 'wind.config.js'], {
 	persistent: true
 })
-	.on('change', path => {
-		console.log(`[dev: watch]: ${path} changed.`);
-		spawn('npm', ['run', 'build:css'], {stdio: 'inherit'});
-	});
+	.on('change',
+		// Add debounce for sometimes you enable `windicss.sortOnSave`
+		debounce(path => {
+			console.log(`[dev: watch]: ${path} changed.`);
+			spawn('npm', ['run', 'build:css'], {stdio: 'inherit'});
+		}, 200, true)
+	);
