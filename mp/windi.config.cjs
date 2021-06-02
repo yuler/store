de0caffb4718a87fcea434b634cf77f253229275
yuler <joyful4yu@gmail.com>
2021-06-03 00:21:47 +0800

    [mp]: Add `app-welcome` component

--- mp/windi.config.cjs
+++ mp/windi.config.cjs
@@ -6,6 +6,21 @@ const {defineConfig} = require('windicss/helpers');
 module.exports = defineConfig({
 	darkMode: 'class', // Or 'media' or 'class'
 	theme: {
-		screens: {}
+		screens: {},
+		extend: {
+			animation: {
+				swing: 'swing .3s linear 2'
+			},
+			keyframes: {
+				swing: {
+					'20%': {transform: 'rotate(10deg)'},
+					'40%': {transform: 'rotate(-10deg)'},
+					'60%': {transform: 'rotate(10deg)'},
+					'80%': {transform: 'rotate(-10deg)'},
+					'100%': {transform: 'rotate(0deg)'}
+				}
+			}
+		}
 	}
+
 });
