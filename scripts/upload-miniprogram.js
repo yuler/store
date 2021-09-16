ca39c76618f3f701aaa3872692e8f65488b02001
yuler <joyful4yu@gmail.com>
2021-09-16 18:35:53 +0800

    ci: add `upload-miniprogram` action

---
+++ scripts/upload-miniprogram.js
@@ -0,0 +1,40 @@
+import fs from 'node:fs/promises'
+import {dirname, resolve} from 'node:path'
+import {fileURLToPath} from 'node:url'
+import CI from 'miniprogram-ci'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const {Project, upload} = CI
+const {appid} = JSON.parse(
+	await fs.readFile(resolve(__dirname, '../project.config.json')),
+)
+const {version, description} = JSON.parse(
+	await fs.readFile(resolve(__dirname, '../package.json')),
+)
+
+const project = new Project({
+	appid,
+	type: 'miniProgram',
+	projectPath: resolve(__dirname, '../mp'),
+	privateKeyPath: './private.key',
+	ignores: ['scripts', 'README.md'],
+})
+
+const result = await upload({
+	project,
+	version,
+	desc: description,
+	setting: {
+		es7: true,
+		minify: true,
+		codeProtect: true,
+		minifyJS: true,
+		minifyWXML: true,
+		minifyWXSS: true,
+		autoPrefixWXSS: true,
+	},
+	onProgressUpdate: console.log,
+})
+
+console.log(result)
